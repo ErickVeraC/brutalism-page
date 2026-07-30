6e287de0b520e790758a1c78b6f0fc48055a6f74
Erick Vera Cuevas <erickverac90@gmail.com>
2026-07-30 13:12:26 -0600

    chore: upgrade to Astro 7 and simplify checks

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
 
 const blog = defineCollection({
 	// Usamos el nuevo loader para Astro v5/v6
